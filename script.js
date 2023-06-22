86b051300c0447c6ae6fc64ae12651044677c73e
Rafaela ✦ <136726196+rafadfreitas@users.noreply.github.com>
2023-06-22 12:11:34 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+ <script>
+        function mostrarNome() {
+            var nome = document.getElementById("nome").value;
+            document.getElementById("resultado").textContent = "O nome digitado foi: " + nome;
+        }
+   </script>
